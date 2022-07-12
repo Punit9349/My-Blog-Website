ae685785388d952aff64572d8a27519346e33c16
Punit9349 <puneetgarg9349@gmail.com>
2022-07-12 13:35:49 +0530

    Adding more styles

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import styles from '../styles/Contact.module.css'
 
 const Contact = () => {
   return (
-    <div>
-        This is our contact page.
-    </div>
+    <>
+
+      <h1 className={styles.h1}>Contact Us</h1>
+      <form>
+         <div className={styles.container}>
+
+          <div className={styles.box}>
+          <label htmlFor="exampleInputEmail1" className={styles.name}>Name   </label>
+          <input type="text" className={styles.fill} id="exampleInputEmail1"       aria-describedby="emailHelp"/>
+          </div>
+         
+          <div className={styles.box}>
+          <label htmlFor="exampleInputEmail1" className={styles.email}>Email Address   </label>
+          <input type="email" className={styles.fill} id="exampleInputEmail1"       aria-describedby="emailHelp"/>
+          </div>
+          
+          <div className={styles.box}>
+          <label htmlFor="exampleInputPassword1" className={styles.phone}>Phone Number  </label>
+          <input type="number" className={styles.fill} id="exampleInputPassword1"/>
+          </div>
+
+          {/* <div class="input-group"> */}
+          <span className={styles.textarea}>Message  </span>
+          <textarea className={styles.textarea1}></textarea>
+          {/* </div> */}
+
+       
+        </div>
+
+        <button type="submit" className={styles.button}>Submit</button>
+      </form>
+    </>
   )
 }
 
---
+++ pages/blogpost/blog1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../../styles/Blog1.module.css'
+
+const blog1 = () => {
+  return (
+    <div>
+        <div className={styles.container}>
+        <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sunt. Consequuntur, laborum voluptas corporis facere doloribus libero cupiditate dolores omnis nulla, temporibus blanditiis et impedit, commodi aliquid. Minus, ipsam unde autem fugiat corrupti doloribus!</p>
+        </div>
+    </div>
+  )
+}
+
+export default blog1
