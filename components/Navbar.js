92b08513cc89ba1eccaeae721a46ed2992ea0758
Punit9349 <puneetgarg9349@gmail.com>
2022-07-25 01:15:39 +0530

    Making navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import Link from 'next/link'
-// import { useState } from 'react'
+import {useState} from 'react'
 import styles1 from '../styles/Navbar.module.css'
 
-const Navbar = () => {
-  var navv=true;
-  const onChange=(e)=>{
-    navv=true;
-    console.log(navv);
-  }
-  return (
-    <div>
+
+const Navbar = () => { 
+   
+      const [boola, setboola] = useState(false);
+      const [func, setfunc] = useState("flex");
+      const [func1, setfunc1] = useState("none");
+      console.log(func1);
+      const onChange=()=>{
+         setboola(true);
+         setfunc('none');
+         setfunc1('flex');
+      }
+
+      const onChange1=()=>{
+        setboola(false);
+        setfunc1('flex');
+        setfunc('none');
+      }
+      return (
+        <div>
           <nav className={styles1.mainnav}  >
           <Link href='/' >
              <a><h3 className={styles1.logo} >TechnoBlogs</h3></a>
-           </Link>
-        <ul className={styles1.menu} style={{display: navv?'flex':'none'}}>
+          </Link>
+        <ul data-visible={boola} className='menu' >
 
          
           <Link href='/' >
@@ -34,16 +46,19 @@ const Navbar = () => {
             <a><li>Contact</li></a>
            </Link>
 
-
+        </ul>
+        <div className={styles1.bumton}> 
           <Link href='/Login' >
             <a><button className={styles1.button}>Login</button></a>
            </Link>
 
           <Link href='/Signup' >
-            <a><button className={styles1.button}>Signup</button></a>
+            <a><button className={styles1.button} id={styles1.button2}>Signup</button></a>
            </Link>
-        </ul>
-           <i className="fa-solid fa-bars fa-xl" id={styles1.icon} onClick={onChange}></i>
+
+         </div>  
+           <i className="fa-solid fa-bars fa-xl" id={styles1.icon} onClick={onChange} style={{display:{func}}}></i>
+           <i className="fa-solid fa-xmark fa-2xl" id={styles1.icon1} onClick={onChange1} style={{display :{func1}}}></i>
       </nav>
     </div>
   )
